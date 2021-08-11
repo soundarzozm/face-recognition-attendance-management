b15e33131a52d1bb041127aa9f994490f77581a9
Soundar Murugan <55850316+soundarzozm@users.noreply.github.com>
2021-08-11 19:59:12 +0530

    Delete creds.py

--- server/web/creds.py
+++
@@ -1,4 +0,0 @@
-AWS_REGION_NAME = "ap-south-1"
-AWS_ACCESS_KEY_ID = "AKIAYHSUX545BVZQTUEI"
-AWS_SECRET_ACCESS_KEY = "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"
-S3_URL = "https://facerecitr.s3.ap-south-1.amazonaws.com/"
